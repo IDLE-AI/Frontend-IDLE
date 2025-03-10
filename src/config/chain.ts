5b8a445ce1bf9fe87a08d11939253d43ae584e8c
zakyirsyaad <zkyxentertain@gmail.com>
2025-03-10 19:07:30 +0700

    feat: add network configuration for Manta and Sonic networks

---
+++ src/config/chain.ts
@@ -0,0 +1,4 @@
+export const mantaNetwork = "https://pacific-rpc.manta.network/http";
+export const sonicNetwork = "https://rpc.blaze.soniclabs.com";
+export const mantaPacificNetwork =
+  "https://pacific-rpc.sepolia-testnet.manta.network/http";
